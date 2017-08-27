fce5d1a714dcf322150b3208eae43832e36e6571
y5RjpEqY <gfxv5e@gmail.com>
2017-08-28 02:41:32 +0900

    広告入れました

--- app/src/main/java/com/cerevo/blueninja/hyourowgan_ble_sample/MainActivity.java
+++ app/src/main/java/com/cerevo/blueninja/hyourowgan_ble_sample/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonShowGPIO;
@@ -13,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
     Button buttonShowMotionSensor;
     Button buttonShowAirpressureSensor;
 
+    AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         buttonShowGPIO = (Button)findViewById(R.id.buttonShowGPIO);
         buttonShowGPIO.setOnClickListener(buttonClickListener);
